4e9a466c41583a06954210915c81b199916e1c6d
web3ten0 <web3ten0@gmail.com>
2023-06-09 21:33:09 +0900

    routesをmountしてみた

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,19 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/")]
+fn news_index() -> &'static str {
+    "News List"
+}
+
+#[get("/<id>")]
+fn news_view(id:u32) -> String {
+    format!("News View {}", id)
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+      .mount("/", routes![index])
+      .mount("/news", routes![news_index, news_view])
 }
