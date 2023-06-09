8e5e3ae23a5c93d18cc08a6670a5eeaf102667b4
web3ten0 <web3ten0@gmail.com>
2023-06-09 22:00:12 +0900

    色々なURLパラメタ

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,19 @@ fn news_view(id:u32) -> String {
     format!("News View {}", id)
 }
 
+#[get("/<name>/<age>/<cool>")]
+fn hoge(name: &str, age: u8, cool:bool) -> String {
+    if cool {
+        format!("COOl {} {}", name, age)
+    } else {
+        format!("HOGE {} {}", name, age)
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
       .mount("/", routes![index])
       .mount("/news", routes![news_index, news_view])
+      .mount("/hoge", routes![hoge])
 }
