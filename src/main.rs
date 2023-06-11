2fa36e7cb96f489cce68035b30803354a13011ab
web3ten0 <web3ten0@gmail.com>
2023-06-11 16:39:29 +0900

    Guardしてみた

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
+use rocket::request::{FromRequest, Outcome};
+use rocket::Request;
+
 #[macro_use] extern crate rocket;
+
+#[derive(Debug)]
+struct Hogehoge(bool);
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for Hogehoge {
+    type Error = std::convert::Infallible;
+
+    async fn from_request(_: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        Outcome::Success(Hogehoge(true))
+    }
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -24,10 +39,15 @@ fn hoge(name: &str, age: u8, cool:bool) -> String {
     }
 }
 
+#[get("/<name>")]
+fn taro(name: &str, _hogehoge: Hogehoge) -> String {
+    format!("Hoge2 {}", name)
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
       .mount("/", routes![index])
       .mount("/news", routes![news_index, news_view])
-      .mount("/hoge", routes![hoge])
+      .mount("/hoge", routes![taro, hoge])
 }
