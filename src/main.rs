7abded0420e7ada6767d21521ce099aca45f59be
net3i <web3ten0@gmail.com>
2023-07-09 22:28:18 +0900

    2つ以上のルートが同じパスとメソッドになっているとエラー

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,22 @@
-use rocket::request::{FromRequest, Outcome};
-use rocket::Request;
-
-#[macro_use] extern crate rocket;
-
-#[derive(Debug)]
-struct Hogehoge(bool);
-
-#[rocket::async_trait]
-impl<'r> FromRequest<'r> for Hogehoge {
-    type Error = std::convert::Infallible;
-
-    async fn from_request(_: &'r Request<'_>) -> Outcome<Self, Self::Error> {
-        Outcome::Success(Hogehoge(true))
-    }
-}
+#[macro_use]
+extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/")]
-fn news_index() -> &'static str {
-    "News List"
+#[get("/<_hoge>")]
+fn hoge(_hoge: u8) -> &'static str {
+    "Hoge!"
 }
 
-#[get("/<id>")]
-fn news_view(id:u32) -> String {
-    format!("News View {}", id)
-}
-
-#[get("/<name>/<age>/<cool>")]
-fn hoge(name: &str, age: u8, cool:bool) -> String {
-    if cool {
-        format!("COOl {} {}", name, age)
-    } else {
-        format!("HOGE {} {}", name, age)
-    }
-}
-
-#[get("/<name>")]
-fn taro(name: &str, _hogehoge: Hogehoge) -> String {
-    format!("Hoge2 {}", name)
+#[get("/<_hoge>")]
+fn hoge2(_hoge: bool) -> &'static str {
+    "Hoge2!"
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-      .mount("/", routes![index])
-      .mount("/news", routes![news_index, news_view])
-      .mount("/hoge", routes![taro, hoge])
+    rocket::build().mount("/", routes![index, hoge, hoge2])
 }
