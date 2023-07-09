ca078e2b4a3f062be78dcd477c92ffe4a366feaf
net3i <web3ten0@gmail.com>
2023-07-10 07:58:38 +0900

    file server

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,20 @@
-#[macro_use]
-extern crate rocket;
+#[macro_use] extern crate rocket;
+
+use std::path::{Path, PathBuf};
+use rocket::fs::NamedFile;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/<_hoge>")]
-fn hoge(_hoge: u8) -> &'static str {
-    "Hoge!"
-}
-
-#[get("/<_hoge>")]
-fn hoge2(_hoge: bool) -> &'static str {
-    "Hoge2!"
+#[get("/<file..>")]
+async fn files(file: PathBuf) -> Option<NamedFile> {
+    let path = Path::new("static/").join(file);
+    NamedFile::open(path).await.ok()
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, hoge, hoge2])
+    rocket::build().mount("/", routes![index, files])
 }
